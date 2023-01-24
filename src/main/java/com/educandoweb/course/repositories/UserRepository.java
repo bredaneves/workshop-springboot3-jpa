e2e54fa2efa826f45d967e4c46bd42089a875a1f
Alexandre B.Neves <bredaneves@gmail.com>
2023-01-24 17:14:04 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoweb.course.entities.User;
 
 //JpaRepository<T, ID> - T objeto e ID tipo
+//@Repository - pode ser colocado, mas nesse caso não é preciso devido a herança
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	//não é preciso implementações nesse caso, pois a extensão do JpaRepository já...
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,37 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	//auto vínculo com o serviço
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		//primeiro parâmetro deve ter o L para indicar que é um long
-		User u = new User(1L, "Maria", "maria@gmail.com", "99998888", "123123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	//mapeamento para o caminho com o id do usuário...
+	//@PathVariable para tornar o id do usuário como parte do caminho
+	@GetMapping (value = "/{id}")	
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
