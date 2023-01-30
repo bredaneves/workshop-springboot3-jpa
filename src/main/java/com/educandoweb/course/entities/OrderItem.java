9b2a61bd4dfc5cc4fd4f108ac8dde65c041f689c
Alexandre B.Neves <bredaneves@gmail.com>
2023-01-30 14:19:40 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,95 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	//faz relacionamento com entidade intermediária...
+	//essa entidade abaixo faz o relacionamento entre tabelas com campos extras
+	//a notação desse tipo de ID é diferente
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {		
+	}
+
+	//o construtor, nesse caso, não deve conter o id, pois será atualizado em outro momento...
+	//além disso, preciso editar manualmente para incluir os campos de objetos (order e product)
+	public OrderItem(Order order, Product product, OrderItemPK id, Integer quantity, Double price) {
+		super();		
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	//os getters and setters também não devem trabalhar com o id nesse momento...
+	//também será preciso criar manulamente para order e product
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	//o hashcode and equals, por outro lado, deve usar somente o id
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
